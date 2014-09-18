ff29c6175f2566454b5f539133dc2960d5f39468
gogotanaka <mail@tanakakazuki.com>
2014-09-18 11:30:56 -0700

    Improve prime.js

--- prime.js
+++ prime.js
@@ -1,21 +1,18 @@
 // alias jsc="/System/Library/Frameworks/JavaScriptCore.framework/Versions/A/Resources/jsc"
 // jsc prime.js
 
-
 function isPrime(element, primes) {
   return primes.every(function(p){
     return element % p != 0;
   });
 }
 
-(function(){
-  var N = 100
-  var primes = [2]
+var N = 100
+var primes = [2]
 
-  for(i = 3; i < N; i++) {
-    if (isPrime(i,primes)) {
-      primes.push(i);
-    }
+for(i = 3; i < N; i++) {
+  if (isPrime(i,primes)) {
+    primes.push(i);
   }
-  print(primes);
-}());
+}
+print(primes);
