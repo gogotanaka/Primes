0bfa682a96cd9f621ade6f0d348db5d8588a1a5c
gogotanaka <mail@tanakakazuki.com>
2014-11-04 20:21:16 -0800

    Refactoring c and C++

--- prime.c
+++ prime.c
@@ -7,7 +7,7 @@ int primes[N];
 int main() {
   for (int i=0; i<N; i++) { primes[i] = 1; }
 
-  for (int sieve=2; sieve<sqrt(N); sieve++) {
+  for (int sieve=2; (sieve^2)<N; sieve++) {
     if (primes[sieve]) {
       for (int j=0; sieve * (j + 2) < N; j++) {
         primes[sieve * (j + 2)] = 0;
