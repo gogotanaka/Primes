6a52428c38594bd5098c260f0e9fa48f2b82bac2
gogotanaka <mail@tanakakazuki.com>
2014-11-06 15:18:23 -0800

    mathematica and refactor ruby

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ N = 100
 
 primes = [*2..N]
 (2..Math.sqrt(N)).each do |sieve|
-  primes.reject! { |num| (num % sieve).zero? && !(num == sieve) }
+  primes.reject! { |num| (num % sieve).zero? && !num.eql?(sieve) }
 end
 
 p primes
