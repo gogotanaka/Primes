e82a5c66de118d57177bab724679eb5223a5438d
kbinani <kbinani.bt@gmail.com>
2014-11-09 22:27:16 +0900

    Optimize prime.f95

--- prime.f95
+++ prime.f95
@@ -1,15 +1,12 @@
 program main
   implicit none
   integer, parameter :: N = 100
-  integer :: sieve = 2, i
+  integer :: sieve, i
   logical :: primes(2:N) = .true.
 
-  do while (sieve ** 2 < N)
-    forall (i = sieve ** 2:N:sieve, primes(sieve))
-      primes(i) = .false.
-    end forall
-    sieve = sieve + 1
-  end do
+  forall (sieve = 2:N, sieve ** 2 < N)
+    primes(sieve ** 2:N:sieve) = .false.
+  end forall
 
   do i = 2, N
     if (primes(i)) then
